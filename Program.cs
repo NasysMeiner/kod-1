17d7ce20041dd8e03ad09d37079eadfcc3cefd04
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-15 23:02:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wh_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPrice = 100;
+            int gold;
+            int numberOfCrystals;
+            int collectCrystals;
+
+            Console.Write("Сколько у вас золота? ");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Сколько требуется кристаллов? ");
+            numberOfCrystals = Convert.ToInt32(Console.ReadLine());
+
+            gold -= numberOfCrystals * crystalPrice;
+
+            Console.Write($"Кристаллов купленно: {numberOfCrystals}, осталось золота: {gold}.");
+            Console.ReadLine();
+        }
+    }
+}
