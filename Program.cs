b44a6eaeb0728207ca36777c7382205c191457d4
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-15 22:31:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wh_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Иванович";
+            string surname ="Иван";
+            string intermediate;
+
+            Console.WriteLine($"Имя: {name}, фамилия: {surname}");
+
+            intermediate = name;
+            name = surname;
+            surname = intermediate;
+
+            Console.WriteLine($"Имя: {name}, фамилия: {surname}");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
