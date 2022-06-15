233f89349db7963d1bb61b3917f21eba3dc0f701
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-15 22:23:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int phots = 52;
+            int capacityRows = 3;
+            int fullRows = phots / capacityRows;
+            int remainderPhotos = phots % capacityRows;
+
+            Console.WriteLine($"Полностью заполненных рядов: {fullRows}, а оставшихся картинок: {remainderPhotos}");
+            Console.ReadLine();
+
+        }
+    }
+}
