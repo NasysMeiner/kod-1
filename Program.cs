63fe92aa71b5e6126e67cd524a4f7d096da31692
NasysMeiner <107574485+NasysMeiner@users.noreply.github.com>
2022-06-16 16:58:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int progress = 0;
+            string message = "";
+            string stop = "exit";
+
+            //Console.WriteLine("Вы хотите продолжить закачку файла?");
+            //message = Console.ReadLine();
+
+            while (message != stop)
+            {
+                Console.WriteLine("Вы хотите продолжить закачку файла?");
+                message = Console.ReadLine();
+                progress++;
+                Console.WriteLine($"Скачалось {progress}кб.");
+            }
+            Console.WriteLine("Вы остановили закачку фала.");
+            Console.ReadLine();
+        }
+    }
+}
